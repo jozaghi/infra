3e76bfb893cd02361c247a18ae39068bb9ec7327
jozaghi <ar.jozaghi@hotmail.com>
2025-07-15 20:08:48 -0700

    configure s3 backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"      # Replace later
-    key            = "global/s3/terraform.tfstate"     # Replace later
-    region         = "us-west-2"
-    dynamodb_table = "my-terraform-lock-table"         # Replace later
-    encrypt        = true
+    bucket = "terraform-state-bucket-arj100"
+    key    = "terraform/terraform.tfstate"
+    region = "us-west-2"
   }
 }
