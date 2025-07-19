745dd5e5228130a92cac4f3b8170ffe90f4b4af0
jozaghi <ar.jozaghi@hotmail.com>
2025-07-19 11:57:06 -0700

    add security group module

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ module "vpc" {
   public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidr = ["10.0.101.0/24", "10.0.102.0/24"]
 }
+
+module "security_group" {
+  source = "./modules/security_group"
+  vpc_id = module.vpc.vpc_id
+  alb_ingress_ports = [80, 443]
+  backend_port = 3000
+  db_port = 5432
+}
